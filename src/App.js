61a95bffd8a13adfdf6792c16efd8154e0eac773
sojoyfulyouth <thdwldbs01@gmail.com>
2024-11-07 09:47:47 +0900

    test creating div for stock

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
 import "./App.css";
 
 function App() {
-  const listEx = ["주식1", "주식2", "주식3", "주식4", "주식5", "주식6"];
+  const TopStock1 = {
+    stockName: "Topstock1",
+    stockLogo: "logo.png",
+    stockPrice: 13000,
+    stockRaise: "13.3%",
+  };
+  const TopStock2 = {
+    stockName: "Topstock2",
+    stockLogo: "logo.png",
+    stockPrice: 13000,
+    stockRaise: "13.3%",
+  };
+  const TopStock3 = {
+    stockName: "Topstock3",
+    stockLogo: "logo.png",
+    stockPrice: 13000,
+    stockRaise: "13.3%",
+  };
+  const TopStockList = [TopStock1, TopStock2, TopStock3];
+
+  const stock1 = {
+    stockName: "stock1",
+    stockLogo: "logo.png",
+    stockPrice: 13000,
+    stockRaise: "13.3%",
+  };
+  const stock2 = {
+    stockName: "stock2",
+    stockLogo: "logo2.png",
+    stockPrice: 2000,
+    stockRaise: "3.7%",
+  };
+  const stockList = [stock1, stock2];
   return (
     <div className="App">
       <h1 id="appTitle"> Catch Stock</h1>
       <div className="top3RankWrap">
-        <div className="top1">Rank1</div>
-        <div className="top2">Rank2</div>
-        <div className="top3">Rank3</div>
+        {TopStockList.map((stock, index) => (
+          <div className="topStock">
+            <div className="TopStockImg">{stock.stockLogo}</div>
+            <span>
+              {stock.stockName}
+              <br />
+              {stock.stockPrice}
+              <br />
+              {stock.stockRaise}
+            </span>
+          </div>
+        ))}
       </div>
       <div className="stockRankingWrap">
-        {listEx.map((stock, index) => (
-          <div key={index} className="stock"></div>
+        {/* 4번째 주식부터 보여줘야함 */}
+        {stockList.map((stock, index) => (
+          <div key={index} className="stock">
+            <span className="stockListContent">
+              <p>{stock.stockLogo}</p>
+              <p>{stock.stockName}</p>
+              <p>{stock.stockPrice}</p>
+              <p>{stock.stockRaise}</p>
+            </span>
+          </div>
         ))}
       </div>
     </div>
