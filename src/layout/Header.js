ce3b42f3db3cec231faaa7a5b09c219818c85082
sojoyfulyouth <thdwldbs01@gmail.com>
2024-12-05 14:05:21 +0900

    call api and test load data

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,40 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./Header.css";
 
 const Header = () => {
-  const TopStock1 = {
-    stockName: "Topstock1",
-    stockLogo: "logo.png",
-    stockPrice: 13000,
-    stockRaise: "13.3%",
+  const [stockInfo, setStockInfo] = useState([]);
+  const url =
+    "https://port-0-kkk-m1jz7al6f2aafb4b.sel4.cloudtype.app/volume-rank";
+  const getStockInfo = () => {
+    axios
+      .get(url)
+      .then((res) => {
+        setStockInfo(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
-  const TopStock2 = {
-    stockName: "Topstock2",
-    stockLogo: "logo.png",
-    stockPrice: 13000,
-    stockRaise: "13.3%",
-  };
-  const TopStock3 = {
-    stockName: "Topstock3",
-    stockLogo: "logo.png",
-    stockPrice: 13000,
-    stockRaise: "13.3%",
-  };
-  const TopStockList = [TopStock1, TopStock2, TopStock3];
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    getStockInfo();
+  }, []);
+
   return (
-    <header>
+    <header className="header">
       <h1 id="appTitle"> Catch Stock</h1>
       <h3 id="appComment">오늘의 인기 주식들이에요 😎</h3>
       <div className="top3RankWrap">
-        {TopStockList.map((stock, index) => (
-          <div className="topStock">
-            <div className="TopStockImg">{stock.stockLogo}</div>
-            <span>
-              {stock.stockName}
-              <br />
-            </span>
-          </div>
-        ))}
+        {[...stockInfo]
+          .filter((stock) => !isNaN(Number(stock.volInrt)))
+          .sort((a, b) => b.volInrt - a.volInrt)
+          .slice(0, 3)
+          .map((stock, index) => (
+            <div className="topStock" key={index}>
+              <span>{stock.htsKorIsnm}</span>
+              <p>{stock.volInrt}%만큼 거래량이 증가했어요!</p>
+            </div>
+          ))}
       </div>
     </header>
   );
